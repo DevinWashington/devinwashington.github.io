dc089b0b44a913e26e287a6437c4da10a3a614ee
DevinW2304 <devbusi4569@gmail.com>
2025-05-04 19:33:44 -0400

    tt

--- itis3135/clientProject/HTMLIncludeClient.js
+++ itis3135/clientProject/HTMLIncludeClient.js
@@ -64,3 +64,19 @@
     w.HTMLInclude();
 }(window, document)
 
+    let slideIndex = 0;
+    function showSlides() {
+        let i;
+        let slides = document.getElementsByClassName("slides");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1;}    
+        slides[slideIndex-1].style.display = "block";  
+        setTimeout(showSlides, 5000); // Change image every 5 seconds
+    }
+    showSlides();
+
+    
+
